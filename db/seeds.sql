17631687f31b75a22c487695f0f865251cbe5902
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-01-06 22:07:19 -0600

    added data to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (id, name)
+VALUES (1, "Sales"), 
+       (2, "Engineering"), 
+       (3, "Finance"), 
+       (4, "Legal");
+
+INSERT INTO role (id, title, salary)
+VALUES (1, "Sales Lead"), 
+       (2, "Salesperson"), 
+       (3, "Lead Engineer"), 
+       (4, "Software Engineer"), 
+       (5, "Account Manager"), 
+       (6, "Accountant"), 
+       (7, "Legal Team Lead"), 
+       (8, "Lawyer");
+
+INSERT INTO employee (id, first_name, last_name)
+VALUES (1, "Sara", "Parker"), 
+(2, "James", "Lewis"), 
+(3, "Adrian", "Hill"), 
+(4, "Barbara", "Mendoza"), 
+(5, "Seth", "Martin"), 
+(5, "Daniel", "Patel"), 
+(5, "Charles", "Jimenez"), 
+(5, "Avery", "Hawkins");
